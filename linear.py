f3e6b7218f90b4e00feff00ecdbb64f8e9dce317
hodit-art <hod_it@miis.ae>
2026-05-05 13:06:26 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 df = pd.read_csv("student_scores.csv")
-X = df['Hours']
-Y = df['Scores']
+X = df.iloc[:, :-1].values  # features
+Y = df.iloc[:, -1].values 
+
 X
